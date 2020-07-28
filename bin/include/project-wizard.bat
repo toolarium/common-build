25f1876e4439571c736c35ca05ff7ba4ee478003
Patrick Meier <patrick.meier@gmx.net>
2020-07-28 10:24:21 +0200

    Reimplemented creating new project

--- bin/include/project-wizard.bat
+++ bin/include/project-wizard.bat
@@ -34,64 +34,91 @@ dir %COMMON_GRADLE_BUILD_URL%\conf\project-types.properties >nul 2>nul
 if %ERRORLEVEL% EQU 0 set CB_PROJECT_CONFIGFILE=%COMMON_GRADLE_BUILD_URL%\conf\project-types.properties
 :VERIFY_PROJECT_CONFIGFILE_END
 if not exist %CB_PROJECT_CONFIGFILE% (echo %CB_LINE% & echo %CB_LINEHEADER%Missing project type configuration file %CB_PROJECT_CONFIGFILE%, please install with the cb-install.bat. & echo %CB_LINE% & goto PROJECT_WIZARD_ERROR_END)
-::echo %CB_LINEHEADER%Use %CB_PROJECT_CONFIGFILE% as project configuration file
+if .%CB_VERBOSE% == .true echo %CB_LINEHEADER%Used configuration file: %CB_PROJECT_CONFIGFILE%
 
 set "CB_PROJECT_CONFIGFILE_TMPFILE=%TEMP%\cb-project-types-%RANDOM%%RANDOM%.tmp"
 type %CB_PROJECT_CONFIGFILE% 2>nul | findstr /C:= > %CB_PROJECT_CONFIGFILE_TMPFILE% 2>nul
 
+set "projectTypeId="
+set "projectType="
 set "projectName="
 set "projectRootPackageName="
+set "projectGroupId="
+set "projectComponentId="
 set "projectDescription="
 
+
 if not .%1==. (set "projectName=%1" & shift)
-if not defined projectName (set projectName=my-project)
-set /p projectName=%CB_LINEHEADER%Please enter project name, e.g. [%projectName%]: 
+set /a "projectTypeId=%projectName%" 2>nul
+if not .%projectTypeId% == . goto VERIFY_PROJECT_TYPE
 
-if not .%1==. (set "projectRootPackageName=%1" & shift)
-if not defined projectRootPackageName (set projectRootPackageName=my.rootpackage.name)
-set /p projectRootPackageName=%CB_LINEHEADER%Please enter package name, e.g. [%projectRootPackageName%]: 
-
-FOR /F "tokens=1,2 delims=-" %%i in ("%projectName%") do ( set "projectGroupId=%%i" )
-set /p projectGroupId=%CB_LINEHEADER%Please enter project group id, e.g. [%projectGroupId%]: 
-
-FOR /F "tokens=1,2 delims=-" %%i in ("%projectName%") do ( set "projectComponentId=%%i" )
-set /p projectComponentId=%CB_LINEHEADER%Please enter project component id, e.g. [%projectComponentId%]: 
-
-if .%0==. goto SET_PROJECT_DESCRIPTION
-set "projectDescription=%1"
-shift
-:CHECK_PROJECT_DESCRIPTION
-if .%1==. goto SET_PROJECT_DESCRIPTION
-set "projectDescription=%projectDescription% %1"
-shift
-goto CHECK_PROJECT_DESCRIPTION
-:SET_PROJECT_DESCRIPTION
-if not defined projectDescription (set projectDescription=The implementation of the %projectName%.)
-set /p projectDescription=%CB_LINEHEADER%Please enter project description [%projectDescription%]: 
-
-echo.
-echo %CB_LINE%
 echo %CB_LINEHEADER%Project type:
-echo %CB_LINE%
-::echo    [1] java-library
-::echo    [2] config project
-::echo.
-::set /p projectTypeId=%CB_LINEHEADER%Please choose the project type [1]: 
-::if .%projectTypeId% == . set projectType=java-library
-::if .%projectTypeId% == .1 set projectType=java-library
-::if .%projectTypeId% == .2 set projectType=config
-echo.
-
 :PRINT_PROJECT_TYPES
-set /a count = 0 & for /f "tokens=1,* delims== " %%i in (%CB_PROJECT_CONFIGFILE_TMPFILE%) do (set /a count += 1 & echo    [!count!] %%i    		%%j)
+set /a count = 0 & for /f "tokens=1,* delims== " %%i in (%CB_PROJECT_CONFIGFILE_TMPFILE%) do (
+	set /a count += 1 
+	for /f "tokens=1,* delims=|" %%a in ("%%j") do set projectTypeName=%%a
+	echo    [!count!] %%i    		!projectTypeName!)
 set projectTypeId=1
 echo.
 set /p projectTypeId=%CB_LINEHEADER%Please choose the project type [1]: 
+:VERIFY_PROJECT_TYPE
 if .%projectTypeId% == . set projectTypeId=1
 set /a count = 0 & for /f "tokens=1,* delims== " %%i in (%CB_PROJECT_CONFIGFILE_TMPFILE%) do (set /a count += 1 & if [!projectTypeId!] == [!count!] set "projectType=%%i")
+set /a count = 0 & for /f "tokens=1,* delims== " %%i in (%CB_PROJECT_CONFIGFILE_TMPFILE%) do (
+	set /a count += 1
+	if [!projectTypeId!] == [!count!] for /f "tokens=1,* delims=|" %%a in ("%%j") do set "projectTypeConfiguration=%%b")
+	
+if not .%projectType% == . if not .%projectName% == . set "projectName=" & echo %CB_LINEHEADER%Project type [%projectType%] & goto END_PROJECT_TYPES
 if .%projectType% == . echo %CB_LINEHEADER%Invalid input %projectTypeId% & echo. & goto PRINT_PROJECT_TYPES
-::echo %projectTypeId% %projectType%
-echo.
+::echo %projectTypeId% %projectType% "%projectTypeConfiguration%"
+:END_PROJECT_TYPES
+
+::echo %projectTypeConfiguration% | findstr /C:"projectName" >nul 2>nul
+::if %ERRORLEVEL% EQU 0 goto SET_PROJECT_NAME_END
+if not .%1==. (set "projectName=%1" & shift)
+if not .%projectName% == . echo %CB_LINEHEADER%Project name [%projectName%] & goto SET_PROJECT_NAME_END
+set projectName=my-project
+set /p projectName=%CB_LINEHEADER%Please enter project name, e.g. [%projectName%]: 
+:SET_PROJECT_NAME_END
+
+echo "%projectTypeConfiguration%" | findstr /C:"projectRootPackageName" >nul 2>nul
+if %ERRORLEVEL% NEQ 0 goto SET_PROJECT_PACKAGENAME_END
+if not .%1==. (set "projectRootPackageName=%1" & shift)
+if not .%projectRootPackageName% == . echo %CB_LINEHEADER%Project package name [%projectRootPackageName%] & goto SET_PROJECT_PACKAGENAME_END
+set projectRootPackageName=my.rootpackage.name
+set /p projectRootPackageName=%CB_LINEHEADER%Please enter package name, e.g. [%projectRootPackageName%]: 
+:SET_PROJECT_PACKAGENAME_END
+
+echo "%projectTypeConfiguration%" | findstr /C:"projectGroupId" >nul 2>nul
+if %ERRORLEVEL% NEQ 0 goto SET_PROJECT_GROUPID_END
+if not .%1==. (set "projectGroupId=%1" & shift)
+if not .%projectGroupId% == . echo %CB_LINEHEADER%Project project group id [%projectGroupId%] & goto SET_PROJECT_GROUPID_END
+FOR /F "tokens=1,2 delims=-" %%i in ("%projectName%") do ( set "projectGroupId=%%i" )
+set /p projectGroupId=%CB_LINEHEADER%Please enter project group id, e.g. [%projectGroupId%]:
+:SET_PROJECT_GROUPID_END
+
+echo "%projectTypeConfiguration%" | findstr /C:"projectComponentId" >nul 2>nul
+if %ERRORLEVEL% NEQ 0 goto SET_PROJECT_COMPONENTID_END
+if not .%1==. (set "projectComponentId=%1" & shift)
+if not .%projectComponentId% == . echo %CB_LINEHEADER%Project project component id [%projectComponentId%] & goto SET_PROJECT_COMPONENTID_END
+FOR /F "tokens=1,2 delims=-" %%i in ("%projectName%") do ( set "projectComponentId=%%i" )
+set /p projectComponentId=%CB_LINEHEADER%Please enter project component id, e.g. [%projectComponentId%]: 
+:SET_PROJECT_COMPONENTID_END
+
+echo "%projectTypeConfiguration%" | findstr /C:"projectDescription" >nul 2>nul
+if %ERRORLEVEL% NEQ 0 goto SET_PROJECT_DESCRIPTION_END
+if .%0==. goto SET_PROJECT_DESCRIPTION
+set "projectDescription=%1" & shift
+:CHECK_PROJECT_DESCRIPTION
+if .%1==. goto SET_PROJECT_DESCRIPTION
+set "projectDescription=%projectDescription% %1" & shift
+goto CHECK_PROJECT_DESCRIPTION
+:SET_PROJECT_DESCRIPTION
+if not ".%projectDescription%" == "." echo %CB_LINEHEADER%Project project description [%projectDescription%] & goto SET_PROJECT_DESCRIPTION_END
+set "projectDescription=The implementation of the %projectName%."
+set /p projectDescription=%CB_LINEHEADER%Please enter project description, e.g. [%projectDescription%]: 
+:SET_PROJECT_DESCRIPTION_END
+if .%CB_VERBOSE% == .true echo %CB_LINEHEADER%Set projectName:%projectName% projectRootPackageName:%projectRootPackageName% projectGroupId:%projectGroupId% projectComponentId:%projectComponentId% projectDescription:"%projectDescription%"
 echo %CB_LINE%
 
 if exist %projectName% goto PROJECT_WIZARD_ERROR_END
@@ -100,6 +127,10 @@ mkdir %projectName% 2>nul
 echo apply from: "https://git.io/JfDQT" > %projectName%\build.gradle
 set "projectStartParameter=--no-daemon"
 goto PROJECT_WIZARD_END
+
+:UNQOTE
+set %1=%~2
+goto :eof
 
 :PROJECT_WIZARD_ERROR_END
 del %CB_PROJECT_CONFIGFILE_TMPFILE% 2>nul
--- bin/include/project-wizard.sh
+++ bin/include/project-wizard.sh
@@ -10,101 +10,219 @@
 #########################################################################
 
 
+CB_PROJECT_CONFIGFILE_TMPFILE=$(mktemp /tmp/cb-project-types.XXXXXXXXX)
+
+
+#########################################################################
+# error handler
+#########################################################################
+errorhandler() {
+    [ -n "$DEBUG" ] && echo "${CB_LINEHEADER}ERROR on line #$LINENO, last command: $BASH_COMMAND"
+    exithandler
+}
+
+
+#########################################################################
+# exit handler
+#########################################################################
+exithandler() {
+	rm "$CB_PROJECT_CONFIGFILE_TMPFILE" >/dev/null 2>&1
+}
+
+
+#########################################################################
+# Read the configuration file
+#########################################################################
+readConfigurationFile() {
+	if [ -n "$CB_CUSTOM_PROJECT_CONFIGFILE" ]; then
+		CB_PROJECT_CONFIGFILE="$CB_CUSTOM_PROJECT_CONFIGFILE"
+	else
+		# default
+		CB_PROJECT_CONFIGFILE="$CB_SCRIPT_PATH/../conf/project-types.properties"
+
+		# if we have a local common gradle build use the project types configuration file
+		if [ -z "$COMMON_GRADLE_BUILD_URL" ]; then
+			[ "$CB_OS" = "cygwin" ] && commonGradleBuildBasePath="$(cygpath $USERPROFILE)/.gradle/common-gradle-build" || commonGradleBuildBasePath="$HOME/.gradle/common-gradle-build"
+			
+			if [ -d "$commonGradleBuildBasePath" ]; then
+				commonGradleBuildVersion=$(find "$commonGradleBuildBasePath" -maxdepth 1 -type d -name "*\.*\.*" -prune -exec ls -d {} \; 2>/dev/null | tail -1 2>/dev/null | xargs -l basename 2>/dev/null)
+				[ -n "$commonGradleBuildVersion" ] && COMMON_GRADLE_BUILD_URL="$commonGradleBuildBasePath/$commonGradleBuildVersion/gradle"
+			fi
+		fi
+
+		[ -r "$COMMON_GRADLE_BUILD_URL/conf/project-types.properties" ] && CB_PROJECT_CONFIGFILE="$COMMON_GRADLE_BUILD_URL/conf/project-types.properties"
+	fi
+
+	if ! [ -r "$CB_PROJECT_CONFIGFILE" ]; then
+		echo "$CB_LINE"
+		echo "${CB_LINEHEADER}Missing project type configuration file $CB_PROJECT_CONFIGFILE, please install with the cb-install.bat."
+		echo "$CB_LINE"
+		endWithError
+	fi
+	
+	[ "$CB_VERBOSE" = "true" ] && echo "${CB_LINEHEADER}Used configuration file: $CB_PROJECT_CONFIGFILE"
+	cat "$CB_PROJECT_CONFIGFILE" 2>/dev/null | grep -v "#" | grep "=" > "$CB_PROJECT_CONFIGFILE_TMPFILE" 2>/dev/null
+}
+
+
+#########################################################################
+# Print project types
+#########################################################################
+printProjectTypes() {
+	typeName=
+	configValue=
+	count=1
+	while IFS= read -r line; do 
+		configValue="${line#*=}"
+		typeName="${configValue%%|*}"
+		
+		echo "   [$count] ${line%=*}  		${typeName}"
+		count=$((count+1))
+	done < "$CB_PROJECT_CONFIGFILE_TMPFILE"
+}
+
+
+#########################################################################
+# Get project type configuration
+#########################################################################
+getProjectTypeConfiguration() {
+	key=
+	configValue=
+	count=1
+	while IFS= read -r line; do
+		configValue="${line#*=}"
+		[ "$count" = "$1" ] && key="${line%=*}" && break
+		count=$((count+1))
+	done < "$CB_PROJECT_CONFIGFILE_TMPFILE"
+	echo "${configValue#*|}" | sed 's/|/\n/g' 2>/dev/null
+}
+
+
+#########################################################################
+# Check if project has the configuraiton
+#########################################################################
+hasProjectTypeConfiguration() {
+	[ -z "$(echo "$projectTypeConfiguration" | grep -E "^$1$")" ] && return 1 || return  0
+}
+
+
+#########################################################################
+# Select the project type
+#########################################################################
+searchProjectType() {
+	key=
+	count=1
+	while IFS= read -r line; do
+		[ "$count" = "$1" ] && key="${line%=*}" && break
+		count=$((count+1))
+	done < "$CB_PROJECT_CONFIGFILE_TMPFILE"
+	echo "$key" 2>/dev/null | sed 's/ //g' 2>/dev/null
+}
+
+
+#########################################################################
+# Select the project type
+#########################################################################
+selectProjectType() {	
+	if [ "$1" -ge 0 ] 2>/dev/null; then
+		projectTypeId="$1"
+		projectType=$(searchProjectType "$1")
+	fi
+		
+	if [ -z "$projectType" ]; then
+		echo "${CB_LINEHEADER}Project type:"
+
+		[ "$1" -ge 0 ] 2>/dev/null && echo "${CB_LINEHEADER}Invalid input $input" && echo ""
+		projectType=
+		input=
+		while [ -z "$projectType" ]; do
+			printProjectTypes
+			
+			echo ""
+			read -p "${CB_LINEHEADER}Please choose the project type [1]: " input
+			[ -z "$input" ] && input=1
+
+			projectType=$(searchProjectType "$input")
+			projectTypeId="$input"
+			[ -z "$projectType" ] && echo "${CB_LINEHEADER}Invalid input $input" && echo ""
+		done
+	else
+		echo "${CB_LINEHEADER}Project type [$projectType]"
+	fi
+	
+	export projectTypeId projectType
+}
+
+
+#########################################################################
+# Select the project type, "project name", "my-project", "own-project"
+#########################################################################
+selectInput() {	
+	if [ -z "$3" ]; then
+		inputResult="$2"	
+		read -p "${CB_LINEHEADER}Please enter $1, e.g. [$inputResult]: " input
+		[ -n "$input" ] && inputResult="$input"
+	else
+		inputResult="$3"
+		echo "${CB_LINEHEADER}${1^} [$3]"
+	fi
+}
+
+
+#########################################################################
+# main
+#########################################################################
+trap 'exithandler $?; exit' 0
+trap 'errorhandler $?; exit' 1 2 3 15
 echo "$CB_LINE"
 echo "${CB_LINEHEADER}Create new project, enter project basic data."
 echo "$CB_LINE"
 
-if [ -n "$CB_CUSTOM_PROJECT_CONFIGFILE" ]; then
-	CB_PROJECT_CONFIGFILE=$CB_CUSTOM_PROJECT_CONFIGFILE
-else
-	# default
-	CB_PROJECT_CONFIGFILE=$CB_SCRIPT_PATH/../conf/project-types.properties
-
-	# if we have a local common gradle build use the project types configuration file
-	if [ -z "$COMMON_GRADLE_BUILD_URL" ]; then
-		[ "$CB_OS" = "cygwin" ] && commonGradleBuildBasePath="$(cygpath $USERPROFILE)/.gradle/common-gradle-build" || commonGradleBuildBasePath="$HOME/.gradle/common-gradle-build"
-		
-		if [ -d "$commonGradleBuildBasePath" ]; then
-			commonGradleBuildVersion=$(find "$commonGradleBuildBasePath" -maxdepth 1 -type d -name "*\.*\.*" -prune -exec ls -d {} \; 2>/dev/null | tail -1 2>/dev/null | xargs -l basename 2>/dev/null)
-			[ -n "$commonGradleBuildVersion" ] && COMMON_GRADLE_BUILD_URL=$commonGradleBuildBasePath/$commonGradleBuildVersion/gradle
-		fi
-	fi
-
-	[ -r "$COMMON_GRADLE_BUILD_URL/conf/project-types.properties" ] && CB_PROJECT_CONFIGFILE=$COMMON_GRADLE_BUILD_URL/conf/project-types.properties
-fi
-
-if ! [ -r "$CB_PROJECT_CONFIGFILE" ]; then
-	echo "$CB_LINE"
-	echo "${CB_LINEHEADER}Missing project type configuration file $CB_PROJECT_CONFIGFILE, please install with the cb-install.bat."
-	echo "$CB_LINE"
-	endWithError
-fi
-echo "${CB_LINEHEADER}Use $CB_PROJECT_CONFIGFILE as project configuration file"
-CB_PROJECT_CONFIGFILE_TMPFILE=$(mktemp /tmp/cb-project-types.XXXXXXXXX)
-cat $CB_PROJECT_CONFIGFILE 2>/dev/null | grep -v "#" | grep "=" > $CB_PROJECT_CONFIGFILE_TMPFILE 2>/dev/null
-
-
+projectTypeId=
+projectType=
 projectName=
 projectRootPackageName=
 projectDescription=
 
-[ -n "$1" ] && projectName=$1 && shift
-[ -z "$projectName" ] && projectName=my-project
-read -p "${CB_LINEHEADER}Please enter project name, e.g. [$projectName]: " input
-[ -n "$input" ] && projectName=$input
+# read the configuration file
+readConfigurationFile
 
-[ -n "$1" ] && projectRootPackageName=$1 && shift
-[ -z "$projectRootPackageName" ] && projectRootPackageName=my.rootpackage.name
-read -p "${CB_LINEHEADER}Please enter package name, e.g. [$projectRootPackageName]: " input
-[ -n "$input" ] && projectRootPackageName=$input
+# if first parameter is a number, then it's the project type
+[ -n "$1" ] && [ "$1" -ge 0 ] 2>/dev/null && projectType=$1 && shift
 
-projectGroupId=${projectName%%-*}
-read -p "${CB_LINEHEADER}Please enter project group id, e.g. [$projectGroupId]: " input
-[ -n "$input" ] && projectGroupId=$input
+# select project type
+selectProjectType "$projectType"
+projectTypeConfiguration=$(getProjectTypeConfiguration "$projectTypeId")
 
-projectComponentId=${projectName%%-*}
-read -p "${CB_LINEHEADER}Please enter project component id, e.g. [$projectComponentId]: " input
-[ -n "$input" ] && projectComponentId=$input
+# select project details
+[ -z "$1" ] && selectInput "project name" "my-project"
+[ -n "$1" ] && selectInput "project name" "my-project" "$1" && shift
+projectName="$inputResult"
 
-[ -n "$*" ] && projectDescription="$*"
-[ -z "$projectDescription" ] && projectDescription="The implementation of the $projectName."
-read -p "${CB_LINEHEADER}Please enter project description [$projectDescription]: " input
-[ -n "$input" ] && projectComponentId=$input
+if hasProjectTypeConfiguration "projectRootPackageName"; then
+	[ -z "$1" ] && selectInput "project package name" "my.rootpackage.name"
+	[ -n "$1" ] && selectInput "project package name" "my.rootpackage.name" "$1" && shift
+	projectRootPackageName="$inputResult"
+fi
 
-echo ""
-echo "$CB_LINE"
-echo "${CB_LINEHEADER}Project type:"
-echo "$CB_LINE"
-echo ""
-#echo "   [1] java-library"
-#echo "   [2] config project"
-#echo ""
-#read -p "${CB_LINEHEADER}Please choose the project type [1]: " input
+if hasProjectTypeConfiguration "projectGroupId"; then
+	[ -z "$1" ] && selectInput "project group id" "${projectName%%-*}"
+	[ -n "$1" ] && selectInput "project group id" "${projectName%%-*}" "$1" && shift
+	projectGroupId="$inputResult"
+fi
 
-projectType=
-while [ -z "$projectType" ]; do
-	count=1
-	while IFS= read -r line; do 
-		echo "   [$count] ${line%=*}  		${line#*=}"
-		count=$((count+1))
-	done <  $CB_PROJECT_CONFIGFILE_TMPFILE
-	
-	echo ""
-	read -p "${CB_LINEHEADER}Please choose the project type [1]: " input
-	
-	[ -z "$input" ] && input=1
-	
-	count=1
-	while IFS= read -r line; do 
-		[ "$count" = "$input" ] && projectType="${line%=*}" && break
-		count=$((count+1))
-	done <  $CB_PROJECT_CONFIGFILE_TMPFILE
-	projectType="$(echo $projectType 2>/dev/null | sed 's/ //g' 2>/dev/null)"
-	[ -z "$projectType" ] && echo "${CB_LINEHEADER}Invalid input $input" && echo ""
-done
-	
-rm -f "$CB_PROJECT_CONFIGFILE_TMPFILE" >/dev/null 2>&1
-echo ""
+if hasProjectTypeConfiguration "projectComponentId"; then
+	[ -z "$1" ] && selectInput "project component id" "${projectName%%-*}"
+	[ -n "$1" ] && selectInput "project component id" "${projectName%%-*}" "$1" && shift
+	projectComponentId="$inputResult"
+fi
+
+if hasProjectTypeConfiguration "projectDescription"; then
+	selectInput "project description" "The implementation of the $projectName." "$*"
+	projectDescription="$inputResult"
+fi
+
+[ "$CB_VERBOSE" = "true" ] && echo "${CB_LINEHEADER}Set projectName:$projectName projectRootPackageName:$projectRootPackageName projectGroupId:$projectGroupId projectComponentId:$projectComponentId projectDescription:$projectDescription%"
 echo "$CB_LINE"
 
 if [ -r "$projectName" ]; then
