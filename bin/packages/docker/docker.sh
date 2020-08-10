99efdc804200958525d603af24c3999643f7dd3b
mepux <patrick.meier@gmx.net>
2020-08-10 15:04:19 +0200

    Docker support

---
+++ bin/packages/docker/docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#########################################################################
+#
+# docker.sh
+#
+# Copyright by toolarium, all rights reserved.
+# MIT License: https://mit-license.org
+#
+#########################################################################
+
+
+CB_PACKAGE_VERSION=
+CB_PACKAGE_DOWNLOAD_NAME=get-docker.sh
+CB_PACKAGE_DOWNLOAD_URL="https://get.docker.com/"
+export CB_PACKAGE_VERSION CB_PACKAGE_DOWNLOAD_NAME CB_PACKAGE_DOWNLOAD_URL
