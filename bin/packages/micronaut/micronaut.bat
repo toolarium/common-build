142da37b66037de70f2373588ac585ac8ec60873
mepux <patrick.meier@gmx.net>
2021-02-14 11:34:42 +0100

    Changed software license to GPL

--- bin/packages/micronaut/micronaut.bat
+++ bin/packages/micronaut/micronaut.bat
@@ -5,7 +5,21 @@
 :: micronaut.bat
 ::
 :: Copyright by toolarium, all rights reserved.
-:: MIT License: https://mit-license.org
+::
+:: This file is part of the toolarium common-build.
+::
+:: The common-build is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+::
+:: The common-build is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+:: GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with Foobar. If not, see <http://www.gnu.org/licenses/>.
 ::
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
