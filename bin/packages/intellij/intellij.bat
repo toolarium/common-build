51d96c68b24868f13dabbf30822632f509f0f9e8
mepux <patrick.meier@gmx.net>
2023-04-25 06:25:27 +0200

    Update intellij

--- bin/packages/intellij/intellij.bat
+++ bin/packages/intellij/intellij.bat
@@ -23,7 +23,7 @@
 ::
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-if not defined CB_INTELLIJ_VERSION set "CB_INTELLIJ_VERSION=2022.3"
+if not defined CB_INTELLIJ_VERSION set "CB_INTELLIJ_VERSION=2023.1"
 set "CB_PACKAGE_VERSION=%1"
 if .%CB_PACKAGE_VERSION%==. set "CB_PACKAGE_VERSION=%CB_INTELLIJ_VERSION%"
 set "CB_PACKAGE_BASE_URL=https://download-cdn.jetbrains.com/idea"
